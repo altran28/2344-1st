f5434fa00ed31021b5767bb8c7c19d4c684eb913
altran28 <89654443+altran28@users.noreply.github.com>
2021-10-12 19:48:00 -0500

    Third Commit for Part C
    This is the last commit for part c, where it will write to the file.

--- Homework2/Code_Problem_2.py
+++ Homework2/Code_Problem_2.py
@@ -4,10 +4,12 @@ Code Problem 2"""
 
 month_list = {"january": "1", "february": "2", "march": "3", "april": "4", "may": "5", "june": "6", "july": "7",
               "august": "8", "september": "9", "october": "10", "november": "11", "december": "12"}
+
+# this is question b
 i = 0
 list2 = []
 list3 = []
-
+# this is question a
 with open("inputDates.txt", "r") as file:
     list1 = []
     for line in file:
@@ -22,7 +24,10 @@ while list1[i] != "-1":
                 date_month = str(month_list[new_lis[0].lower()])
                 date_day = str(new_lis[1][:-1])
                 date_year = str(new_lis[2])
-                new_date = date_month + '/' + date_day + '/' + date_year
+                new_date = date_month + '/' + date_day + '/' + date_year + '\n'
                 list2.append(new_date)
     i += 1
-print('\n'.join(list2))
+
+file2 = open('parsedDates.txt', 'w+')
+file2.writelines(list2)
+file2.close()
